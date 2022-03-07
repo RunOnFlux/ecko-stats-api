ee92a98dae927ccc9b4af5e9d386bd9bb6732750
Antonio Semeraro <antsemeraro@gmail.com>
2022-03-07 21:39:04 +0100

    SSL enabled

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,32 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestApplication, NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let httpsOptions = null;
+  if (process.env.SSL_PRIV_KEY_PEM && process.env.SSL_FULL_CHAIN_PEM) {
+    const privKeyPath = join(__dirname, process.env.SSL_PRIV_KEY_PEM);
+    const fullChainPath = join(__dirname, process.env.SSL_FULL_CHAIN_PEM);
+    httpsOptions = {
+      key: fs.readFileSync(privKeyPath),
+      cert: fs.readFileSync(fullChainPath),
+    };
+    Logger.log(
+      `SSL_PRIV_KEY_PEM at ${privKeyPath} ${
+        fs.existsSync(privKeyPath) ? 'FOUND' : 'NOT FOUND'
+      }`,
+    );
+    Logger.log(
+      `SSL_FULL_CHAIN_PEM at ${fullChainPath} ${
+        fs.existsSync(fullChainPath) ? 'FOUND' : 'NOT FOUND'
+      }`,
+    );
+  }
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Kaddex Stats API')
