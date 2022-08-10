8bdf78fde78f4b9e48271eca23b8ed5f7dcd1a39
Antonio Semeraro <antsemeraro@gmail.com>
2022-08-11 00:07:41 +0200

    Get TVL from pact

---
+++ src/data/tokens.ts
@@ -0,0 +1,79 @@
+export const PAIR_TOKENS = {
+  'coin:runonflux.flux': {
+    name: 'coin:runonflux.flux',
+    token0: {
+      name: 'KDA',
+      code: 'coin',
+    },
+    token1: {
+      name: 'FLUX',
+      code: 'runonflux.flux',
+    },
+  },
+  'coin:hypercent.prod-hype-coin': {
+    name: 'coin:hypercent.prod-hype-coin',
+    token0: {
+      name: 'KDA',
+      code: 'coin',
+    },
+    token1: {
+      name: 'HYPE',
+      code: 'hypercent.prod-hype-coin',
+    },
+  },
+  'coin:mok.token': {
+    name: 'coin:mok.token',
+    token0: {
+      name: 'KDA',
+      code: 'coin',
+    },
+    token1: {
+      name: 'MOK',
+      code: 'mok.token',
+    },
+  },
+  'coin:lago.kwUSDC': {
+    name: 'coin:lago.kwUSDC',
+    token0: {
+      name: 'KDA',
+      code: 'coin',
+    },
+    token1: {
+      name: 'USDC',
+      code: 'lago.kwUSDC',
+    },
+  },
+  'coin:kaddex.kdx': {
+    name: 'coin:kaddex.kdx',
+    token0: {
+      name: 'KDA',
+      code: 'coin',
+    },
+    token1: {
+      name: 'KDX',
+      code: 'kaddex.kdx',
+    },
+  },
+  'coin:kdlaunch.token': {
+    name: 'coin:kdlaunch.token',
+    token0: {
+      name: 'KDA',
+      code: 'coin',
+    },
+    token1: {
+      name: 'KDL',
+      code: 'kdlaunch.token',
+    },
+  },
+  'coin:kdlaunch.kdswap-token': {
+    name: 'coin:kdlaunch.kdswap-token',
+    token0: {
+      name: 'KDA',
+      code: 'coin',
+    },
+    token1: {
+      name: 'KDS',
+      code: 'kdlaunch.kdswap-token',
+    },
+  },
+};
--- src/utils/chainweb.util.ts
+++ src/utils/chainweb.util.ts
@@ -1,4 +1,7 @@
 import { IRefData } from 'src/interfaces/kswap.exchange.SWAP.interface';
+import * as pact from 'pact-lang-api';
+import { PAIR_TOKENS } from 'src/data/tokens';
+import { IKaddexTokenSupply } from 'src/interfaces/kaddex-token-supply.interface';
 
 export const getApiBalance = (apiBalance) => {
   let balance = 0;
@@ -14,4 +17,52 @@ export const CHAINWEB_ESTATS_URL = 'https://estats.chainweb.com/txs/events';
 
 export const isKdaCoin = (refData: IRefData): boolean => {
   return refData.refName.namespace === null && refData.refName.name === 'coin';
+};
+
+export const pactFetchLocal = async (chainId: number, pactCode: string) => {
+  const networkId = process.env.CHAINWEB_NETWORK_ID;
+  const url = `${process.env.CHAINWEB_NODE_URL}/chainweb/0.0/${networkId}/chain/${chainId}/pact`;
+  return await pact.fetch.local(
+    {
+      pactCode,
+      meta: pact.lang.mkMeta(
+        '',
+        chainId.toString(),
+        0.0000001,
+        150000,
+        Math.round(new Date().getTime() / 1000) - 10,
+        600,
+      ),
+    },
+    url,
+  );
+};
+
+export const getPairsTVL = async (
+  chainId: number,
+): Promise<IKaddexTokenSupply[]> => {
+  const pairList = await Promise.all(
+    Object.values(PAIR_TOKENS).map(
+      async (pair): Promise<IKaddexTokenSupply> => {
+        const pactCode = `
+      (use kaddex.exchange)
+      (let*
+        (
+          (p (get-pair ${pair.token0.code} ${pair.token1.code}))
+          (reserveA (reserve-for p ${pair.token0.code}))
+          (reserveB (reserve-for p ${pair.token1.code}))
+          (totalBal (kaddex.tokens.total-supply (kaddex.exchange.get-pair-key ${pair.token0.code} ${pair.token1.code})))
+        )[totalBal reserveA reserveB])
+       `;
+        const data = await pactFetchLocal(chainId, pactCode);
+        return {
+          tokenFrom: pair.token0.code,
+          tokenTo: pair.token1.code,
+          tokenFromTVL: getApiBalance(data.result.data[1]),
+          tokenToTVL: getApiBalance(data.result.data[2]),
+        };
+      },
+    ),
+  );
+  return pairList;
 };
