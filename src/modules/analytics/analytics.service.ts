22cd8359fa569a4672fa4e0fd7b7b29ba75c1e03
Giuseppe Pace <pacepeppe2003@libero.it>
2022-10-27 18:03:20 +0200

    Updated circulating supply formula

--- src/modules/analytics/analytics.service.ts
+++ src/modules/analytics/analytics.service.ts
@@ -65,8 +65,7 @@ export class AnalyticsService {
       const pactResponse = await pact.fetch.local(
         {
           pactCode: `(let* (
-                  (total-supply (- (kaddex.kdx.total-supply) (+ (at 'total-minted (kaddex.kdx.get-raw-supply 'network-rewards)) 
-                    (- (at 'total-minted (kaddex.kdx.get-raw-supply 'network-rewards)) (kaddex.kdx.get-balance 'kaddex-kdx-wrapper-mint-bank)))))
+                  (total-supply (- (kaddex.kdx.total-supply) (kaddex.kdx.get-balance 'kaddex-kdx-wrapper-mint-bank)))
                   (staking-data (map (kaddex.staking.get-stake-record) (keys kaddex.staking.stake-table)))
                   (staked-kdx (at 'staked-kdx (kaddex.staking.get-pool-state)))
                  ){'total-supply:total-supply, 'staking-data:staking-data, 'staked-kdx:staked-kdx})`,
