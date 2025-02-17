da4711fa16b196698bc60f7eb4b0898b6af776e6
Antonio Semeraro <antsemeraro@gmail.com>
2025-02-17 16:25:31 +0100

    Implement hybrid approach for fetching chain tokens
    - Added a new function `getChainTokensBatch` that attempts to retrieve fungible tokens in one shot using a single Pact code execution
    - If the batch approach fails (e.g., due to a "ghost" module), we fall back to `getChainTokens` which iterates modules individually
    - Improves performance on chains where a batch call succeeds, while remaining robust against modules that could block the entire batch fetch
    - Updated logs and error handling

--- src/modules/chain-data/chain-data.service.ts
+++ src/modules/chain-data/chain-data.service.ts
@@ -1,7 +1,7 @@
 import { ConsoleLogger, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import * as pact from 'pact-lang-api';
+import * as Pact from 'pact-lang-api';
 import { ChainData, ChainDataDocument } from './schemas/chain-data.schema';
 import { whiteListedTokens } from './fixture/tokens';
 
@@ -28,60 +28,23 @@ export class ChainDataService {
 
   async fetchChainsFungibleTokens() {
     this.logger.log('START CHAIN FUNGIBLE TOKENS IMPORT');
-    const pactCode = `(let
-        ((all-tokens
-           (lambda (contract:object)
-             (let*
-               ((module-name (at 'name contract))
-                (interfaces (if (contains 'interfaces contract) (at 'interfaces contract) (if (contains 'interface contract) (at 'interface contract) [])))
-                (is-implementing-fungible-v2 (contains "fungible-v2" interfaces))
-               )
-             (if is-implementing-fungible-v2 module-name "")
-             )
-           )
-         )
-        )
-        (filter (!= "") (map (all-tokens) (map (describe-module) (list-modules))))
-      )`;
-    const chains = Array.from(Array(20).keys());
+
+    const chains = Array.from(Array(20).keys()); // [0..19]
     const fungibleTokens: string[][] = [];
+
     for (const chainId of chains) {
-      try {
-        const pactResponse = await pact.fetch.local(
-          {
-            pactCode,
-            meta: pact.lang.mkMeta(
-              '',
-              chainId.toString(),
-              0.0000001,
-              15000000,
-              Math.round(new Date().getTime() / 1000) - 10,
-              600,
-            ),
-          },
-          `${process.env.CHAINWEB_NODE_URL}/chainweb/0.0/${process.env.CHAINWEB_NETWORK_ID}/chain/${chainId}/pact`,
-        );
-        if (pactResponse?.result?.status === 'success') {
-          const chainTokens: string[] = pactResponse?.result?.data;
-          this.logger.log(
-            `Fetch completed for chain ${chainId} (${chainTokens?.length} fung-tokens)`,
-          );
-          fungibleTokens.push(chainTokens);
-        } else {
-          this.logger.warn(`Unable to fetch for chain ${chainId}`);
-          this.logger.warn(pactResponse?.result?.error?.message);
-          fungibleTokens.push([]);
-        }
-      } catch (error) {
-        this.logger.error(error);
-        fungibleTokens.push([]);
+      let chainTokens = await this.getChainTokensBatch(chainId?.toString());
+      if (!chainTokens) {
+        this.logger.error(`Batch token fetching failed for chain ${chainId}`);
+        chainTokens = await this.getChainTokens(chainId?.toString());
       }
+      console.log(`CHAIN ${chainId} chainTokens:`, chainTokens.length);
+      fungibleTokens.push(chainTokens);
     }
+
     const founded = await this.chainDataModel.findOneAndUpdate(
       {},
-      {
-        fungibleTokens,
-      },
+      { fungibleTokens },
     );
 
     if (!founded) {
@@ -89,6 +52,152 @@ export class ChainDataService {
         fungibleTokens,
       };
       await this.chainDataModel.create(chainData);
+    }
+  }
+
+  async getChainTokens(chainId: string): Promise<string[]> {
+    try {
+      const listModulesCode = '(list-modules)';
+      const listModulesResponse = await Pact.fetch.local(
+        {
+          pactCode: listModulesCode,
+          meta: Pact.lang.mkMeta(
+            '',
+            chainId.toString(),
+            0.0000001,
+            15000000,
+            Math.round(new Date().getTime() / 1000) - 10,
+            600,
+          ),
+        },
+        `${process.env.CHAINWEB_NODE_URL}/chainweb/0.0/${process.env.CHAINWEB_NETWORK_ID}/chain/${chainId}/pact`,
+      );
+
+      if (listModulesResponse?.result?.status === 'failure') {
+        this.logger.error(
+          `[CHAIN ${chainId}] Error calling list-modules: ${listModulesResponse?.result?.message}`,
+        );
+        return null;
+      } else {
+        this.logger.debug(
+          `[CHAIN ${chainId}] Founded ${listModulesResponse?.result?.data?.length} modules`,
+        );
+      }
+
+      if (listModulesResponse?.result?.status === 'success') {
+        const modules = listModulesResponse?.result?.data as string[];
+
+        const chainTokens: string[] = [];
+        let moduleCount = 1;
+        for (const mod of modules) {
+          const describeModuleCode = `(describe-module "${mod}")`;
+          try {
+            this.logger.log(
+              `[CHAIN ${chainId}] Checking module ${moduleCount}/${modules.length} ${mod} on chain ${chainId}`,
+            );
+            const describeModuleResponse = await Pact.fetch.local(
+              {
+                pactCode: describeModuleCode,
+                meta: Pact.lang.mkMeta(
+                  '',
+                  chainId,
+                  0.0000001,
+                  15000000,
+                  Math.round(new Date().getTime() / 1000) - 10,
+                  600,
+                ),
+              },
+              `${process.env.CHAINWEB_NODE_URL}/chainweb/0.0/${process.env.CHAINWEB_NETWORK_ID}/chain/${chainId}/pact`,
+            );
+
+            if (describeModuleResponse?.result?.status === 'success') {
+              const descr = describeModuleResponse?.result?.data;
+              const interfaces = descr.interfaces || descr.interface || [];
+
+              if (interfaces.includes('fungible-v2')) {
+                this.logger.log(
+                  `[CHAIN ${chainId}] Found fungible token ${descr.name}`,
+                );
+                chainTokens.push(descr.name);
+              } else {
+                this.logger.debug(
+                  `[CHAIN ${chainId}] MODULE NOT SUPPORTED! ${mod}: ${describeModuleResponse?.result?.error?.message}`,
+                );
+              }
+            } else {
+              this.logger.debug(
+                `[CHAIN ${chainId}] MODULE NOT SUPPORTED! ${mod}: ${describeModuleResponse?.result?.error?.message}`,
+              );
+            }
+          } catch (describeErr) {
+            this.logger.debug(
+              `[CHAIN ${chainId}] MODULE NOT SUPPORTED! ${mod}`,
+            );
+          }
+          moduleCount++;
+        }
+
+        this.logger.log(
+          `Fetch completed for chain ${chainId} (${chainTokens.length} fung-tokens)`,
+        );
+
+        return chainTokens;
+      } else {
+        this.logger.warn(`Unable to fetch modules for chain ${chainId}`);
+        this.logger.warn(listModulesResponse?.result?.error?.message);
+        return null;
+      }
+    } catch (error) {
+      this.logger.error(error);
+      return null;
+    }
+  }
+
+  async getChainTokensBatch(chainId: string): Promise<string[]> {
+    const pactCode = `(let
+    ((all-tokens
+       (lambda (contract:object)
+         (let*
+           ((module-name (at 'name contract))
+            (interfaces (if (contains 'interfaces contract) (at 'interfaces contract) (if (contains 'interface contract) (at 'interface contract) [])))
+            (is-implementing-fungible-v2 (contains "fungible-v2" interfaces))
+           )
+         (if is-implementing-fungible-v2 module-name "")
+         )
+       )
+     )
+    )
+    (filter (!= "") (map (all-tokens) (map (describe-module) (list-modules))))
+  )`;
+    try {
+      const pactResponse = await Pact.fetch.local(
+        {
+          pactCode,
+          meta: Pact.lang.mkMeta(
+            '',
+            chainId.toString(),
+            0.0000001,
+            15000000,
+            Math.round(new Date().getTime() / 1000) - 10,
+            600,
+          ),
+        },
+        `${process.env.CHAINWEB_NODE_URL}/chainweb/0.0/${process.env.CHAINWEB_NETWORK_ID}/chain/${chainId}/pact`,
+      );
+      if (pactResponse?.result?.status === 'success') {
+        const chainTokens: string[] = pactResponse?.result?.data;
+        this.logger.log(
+          `Fetch completed for chain ${chainId} (${chainTokens?.length} fung-tokens)`,
+        );
+        return chainTokens;
+      } else {
+        this.logger.warn(`Unable to fetch for chain ${chainId}`);
+        this.logger.warn(pactResponse?.result?.error?.message);
+        return null;
+      }
+    } catch (error) {
+      this.logger.error(error);
+      return null;
     }
   }
 }
